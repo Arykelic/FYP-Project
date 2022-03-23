64f127c745d1a6654d1d29e77e490c7b623f7909
Dominic Loh <dominic.loh@hotmail.com>
2022-03-23 17:53:18 +0800

    Updated all web scraper files

--- CatalogueScraper.py
+++ CatalogueScraper.py
@@ -1,62 +1,76 @@
-from urllib.request import urlopen as uReq
-from bs4 import BeautifulSoup as soup
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
 
+s = HTMLSession()
 search_term = "samsung+smartphones"
-my_url = "https://www.amazon.sg/s?k={}".format(search_term)
-my_url
+url = "https://www.amazon.sg/s?k={}".format(search_term)
 
-#opening connection to url and grabbing page
-uClient = uReq(my_url)
-page_html = uClient.read()
-uClient.close()
+def getdata(url):
+    r = s.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    return soup
 
-#html parsing
-page_soup = soup(page_html, "html.parser")
-
-#use below line to check the html set
-#page_soup.h1
-
-#pulling all data sets on current page and verifying length
-containers = page_soup.findAll("div",{"class":"sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 sg-col s-widget-spacing-small sg-col-4-of-20"})
-#use below line to check the length of the dataset
-#len(containers)
-#containers
+def getnextpage(soup):
+    page = soup.find("span", {"class":"s-pagination-strip"})
+    if not page.find("span", {"class":"s-pagination-item s-pagination-next s-pagination-disabled "}):
+        url = "https://www.amazon.sg" + str(page.find("a",{"class":"s-pagination-item s-pagination-next s-pagination-button s-pagination-separator"})["href"])
+        return url
+    else:
+        return
 
 filename = "{}_Catalogue.csv".format(search_term)
-f = open(filename, "w")
+f = open(filename, "w", encoding="utf-8")
 
 headers = "Image_Url, Item_Name, Item_Price, Average_Rating, Number_Review\n"
 
 f.write(headers)
-
-#loop
-for container in containers:
-        Image_Url_Container = container.findAll("div", {"class":"a-section aok-relative s-image-square-aspect"})
-        Image_Url = Image_Url_Container[0].img["src"]
-
-        Item_Name_Container = container.findAll("span", {"class":"a-size-base-plus a-color-base a-text-normal"})
-        Item_Name = Item_Name_Container[0].text
-
-        try:
-            Item_Price_Container = container.findAll("span", {"class":"a-offscreen"})
-            Item_Price = Item_Price_Container[0].text
-        
-            Average_Rating_Container = container.findAll("span", {"class":"a-icon-alt"})
-            Average_Rating = Average_Rating_Container[0].text[0:4]
-
-            Number_Review_Container = container.findAll("span", {"class":"a-size-base s-underline-text"})
-            Number_Review = Number_Review_Container[0].text
-
-        except:
-            Average_Rating = ""
-            Number_Review = ""
-
-        print("Image Url: " + Image_Url)
-        print("Item_Name: " + Item_Name)
-        print("Item_Price: " + Item_Price)
-        print("Average_Rating: " + Average_Rating)
-        print("Number_Review: " + Number_Review)
     
-        f.write(Image_Url.replace(",", "|") + "," + Item_Name.replace(",", "|") + "," + Item_Price.replace(",", "'") + "," + Average_Rating.replace(",", "'") + "," + Number_Review.replace(",", "'") + "\n")
+while True:
+    try:
+        soup = getdata(url)
+        #pulling all data sets on current page and verifying length
+        containers = soup.findAll("div",{"class":"sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 sg-col s-widget-spacing-small sg-col-4-of-20"})
+        #use below line to check the length of the dataset
+        #len(containers)
+        #containers
+        #filename = "{}_Catalogue.csv".format(url).replace("/",",").replace("?",",")
+        #f = open(filename, "w", encoding="utf-8")
 
-f.close()
+        #headers = "Image_Url, Item_Name, Item_Price, Average_Rating, Number_Review\n"
+
+        #f.write(headers)
+
+        #loop
+        for container in containers:
+                Image_Url_Container = container.findAll("div", {"class":"a-section aok-relative s-image-square-aspect"})
+                Image_Url = Image_Url_Container[0].img["src"]
+
+                Item_Name_Container = container.findAll("span", {"class":"a-size-base-plus a-color-base a-text-normal"})
+                Item_Name = Item_Name_Container[0].text
+
+                try:
+                    Item_Price_Container = container.findAll("span", {"class":"a-offscreen"})
+                    Item_Price = Item_Price_Container[0].text
+                
+                    Average_Rating_Container = container.findAll("span", {"class":"a-icon-alt"})
+                    Average_Rating = Average_Rating_Container[0].text[0:4]
+
+                    Number_Review_Container = container.findAll("span", {"class":"a-size-base s-underline-text"})
+                    Number_Review = Number_Review_Container[0].text
+
+                except:
+                    Average_Rating = ""
+                    Number_Review = ""
+
+                print("Image Url: " + Image_Url)
+                print("Item_Name: " + Item_Name)
+                print("Item_Price: " + Item_Price)
+                print("Average_Rating: " + Average_Rating)
+                print("Number_Review: " + Number_Review)
+            
+                f.write(Image_Url.replace(",", "|") + "," + Item_Name.replace(",", "|") + "," + Item_Price.replace(",", "'") + "," + Average_Rating.replace(",", "'") + "," + Number_Review.replace(",", "'") + "\n")
+        
+        url = getnextpage(soup)
+    except TypeError:
+        f.close()
+        print("End of CSV Writing")
--- ReviewScraper.py
+++ ReviewScraper.py
@@ -1,68 +1,80 @@
-from urllib.request import urlopen as uReq
-from bs4 import BeautifulSoup as soup
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
 
-my_url = "https://www.amazon.sg/Samsung-Factory-Unlocked-Smartphone-Pro-Grade/product-reviews/B08FYTSXGQ/ref=cm_cr_getr_d_paging_btm_prev_1?ie=UTF8&reviewerType=all_reviews&pageNumber=1"
-my_url
+s = HTMLSession()
+url = "https://www.amazon.sg/Samsung-Factory-Unlocked-Smartphone-Pro-Grade/product-reviews/B08FYTSXGQ/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews"
 
-# opening connection to url and grabbing page
+def getdata(url):
+    r = s.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    return soup
 
-uClient = uReq(my_url)
-page_html = uClient.read()
-uClient.close()
+def getnextpage(soup):
+    page = soup.find("ul", {"class":"a-pagination"})
+    if not page.find("li", {"class":"a-disabled a-last"}):
+        url = "https://www.amazon.sg" + str(page.find("li",{"class":"a-last"}).find("a")["href"])
+        return url
+    else:
+        return
 
-# html parsing
-page_soup = soup(page_html, "html.parser")
+soup = getdata(url)
+search_term_value = soup.find("h1", {"class":"a-size-large a-text-ellipsis"}).text
+search_term = search_term_value.strip().replace('"', ",").replace("|",",")
 
-# use below line to check the html set
-# page_soup.h1
-
-# pulling all data sets on current page and verifying length
-containers = page_soup.findAll("div", {"class": "a-section review aok-relative"})
-# use below line to check the length of the dataset
-# len(containers)
-
-search_term_value = page_soup.find("h1", {"class": "a-size-large a-text-ellipsis"}).text
-search_term_stripped = search_term_value.strip()
-search_term = search_term_stripped.replace('"', ",").replace("|",",")
-search_term
-
-filename = "{}_Page.csv".format(search_term)
-f = open(filename, "w")
+filename = "{}_Reviews.csv".format(search_term)
+f = open(filename, "w", encoding="utf-8")
 
 headers = "Image_Url, Item_Name, Username, Rating_Score, Review_Description, Review_Date \n"
 
 f.write(headers)
+    
+while True:
+    
+        soup = getdata(url)
+        #pulling all data sets on current page and verifying length
+        containers = soup.findAll("div", {"class": "a-section review aok-relative"})
+        #use below line to check the length of the dataset
+        #len(containers)
+        #containers
+        #filename = "{}_Catalogue.csv".format(url).replace("/",",").replace("?",",")
+        #f = open(filename, "w", encoding="utf-8")
+
+        #headers = "Image_Url, Item_Name, Item_Price, Average_Rating, Number_Review\n"
+
+        #f.write(headers)
+
+        #loop
+        for container in containers:
+                Image_Url = soup.find("div", {"class": "a-text-center a-spacing-top-micro a-fixed-left-grid-col product-image a-col-left"}).img["src"]
+
+                Item_Name_Value = soup.find("h1", {"class": "a-size-large a-text-ellipsis"}).text
+                Item_Name = Item_Name_Value.strip()
+
+                Username_Container = container.findAll("span", {"class": "a-profile-name"})
+                Username = Username_Container[0].text
+
+                Rating_Score_Container = container.findAll("span", {"class": "a-icon-alt"})
+                Rating_Score = Rating_Score_Container[0].text[0:4]
+
+                Review_Description_Container = container.findAll("div", {"class": "a-row a-spacing-small review-data"})
+                Review_Description_Value = Review_Description_Container[0].text
+                Review_Description = Review_Description_Value.strip()
+
+                Review_Date_Container = container.findAll("span", {"class": "a-size-base a-color-secondary review-date"})
+                Review_Date = Review_Date_Container[0].text
 
 
-# loop
-for container in containers:
-    Image_Url = page_soup.find("div", {"class": "a-text-center a-spacing-top-micro a-fixed-left-grid-col product-image a-col-left"}).img["src"]
+                print("Image Url: " + Image_Url)
+                print("Item_Name: " + Item_Name)
+                print("Username: " + Username)
+                print("Rating_Score: " + Rating_Score)
+                print("Review_Description: " + Review_Description)
+                print("Review_Date: " + Review_Date)
 
-    Item_Name_Value = page_soup.find("h1", {"class": "a-size-large a-text-ellipsis"}).text
-    Item_Name = Item_Name_Value.strip()
-
-    Username_Container = container.findAll("span", {"class": "a-profile-name"})
-    Username = Username_Container[0].text
-
-    Rating_Score_Container = container.findAll("span", {"class": "a-icon-alt"})
-    Rating_Score = Rating_Score_Container[0].text[0:4]
-
-    Review_Description_Container = container.findAll("div", {"class": "a-row a-spacing-small review-data"})
-    Review_Description_Value = Review_Description_Container[0].text
-    Review_Description = Review_Description_Value.strip()
-
-    Review_Date_Container = container.findAll("span", {"class": "a-size-base a-color-secondary review-date"})
-    Review_Date = Review_Date_Container[0].text
-
-
-    print("Image Url: " + Image_Url)
-    print("Item_Name: " + Item_Name)
-    print("Username: " + Username)
-    print("Rating_Score: " + Rating_Score)
-    print("Review_Description: " + Review_Description)
-    print("Review_Date: " + Review_Date)
-
-    f.write(Image_Url.replace(",", "|") + "," + Item_Name.replace(",", "|") + "," + Username.replace(",", ".") 
-    + "," + Rating_Score.replace(",", ".") + "," + Review_Description.replace(",", "'").replace("\U0001f60a",":)") + "," + Review_Date.replace(",", "'") + "\n")
-
-f.close()
+                f.write(Image_Url.replace(",", "|") + "," + Item_Name.replace(",", "|") + "," + Username.replace(",", ".") 
+                + "," + Rating_Score.replace(",", ".") + "," + Review_Description.replace(",", "'").replace("\U0001f60a",":)") + "," + Review_Date.replace(",", "'") + "\n")
+        
+        url = getnextpage(soup)
+        if not url:
+            f.close()
+            print("End of CSV Writing")
