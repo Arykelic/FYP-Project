e78ab3e2cc1eca40e39b7769e9663452b0fa838a
Dominic Loh <dominic.loh@hotmail.com>
2022-04-17 05:12:15 +0800

    PHP Composer Push

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3f0d8787d02f9b1f046fd9d9ac003608b8a11d81',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3f0d8787d02f9b1f046fd9d9ac003608b8a11d81',
+            'dev_requirement' => false,
+        ),
+    ),
+);
