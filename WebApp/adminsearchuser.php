b0038123e105215f6c40270731e8f6b3219a72e1
Dominic Loh <dominic.loh@hotmail.com>
2022-04-27 07:54:43 +0800

    test production

--- WebApp/adminsearchuser.php
+++ WebApp/adminsearchuser.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || $_SESSION
 }
 
 include "GlobalClass.php";
-include "UserConfig.php";
+
 
 if (isset($_POST['searchValue'])) {
     $searchValue = $_POST['searchValue'];
