235be3822871889d29a32cb0af59e23617757358
Dominic Loh <dominic.loh@hotmail.com>
2022-04-27 15:25:43 +0800

    update scraper

--- AmazonSG Web Scraper/AmazonSGImportCatalogueData.py
+++ AmazonSG Web Scraper/AmazonSGImportCatalogueData.py
@@ -2,10 +2,10 @@ from debugpy import connect
 import pymysql
 import pandas as pd
 
-empdata = pd.read_csv('C:\\xampp\\htdocs\\FYP-Project\\AmazonSG Catalogue Files\\*.csv', index_col=False, delimiter = ',')
-empdata["Item_Price"].fillna("NA", inplace = True) 
-empdata[" Average Rating_Score (Max Score is 5)"].fillna("NA", inplace = True) 
-empdata["Number_Of_Reviews"].fillna("NA", inplace = True) 
+empdata = pd.read_csv('C://xampp//htdocs//FYP-Project//AmazonSG Catalogue Files//*.csv', index_col=False, delimiter = ',')
+empdata[" item_price"].fillna("NA", inplace = True) 
+empdata[" average_rating (Max Score is 5)"].fillna("NA", inplace = True) 
+empdata[" number_of_ratings "].fillna("NA", inplace = True) 
 empdata.head()
 
 connection = pymysql.connect(host="remotemysql.com", user="y0vryqAKXK", passwd="moMOpaacUP", database="y0vryqAKXK")
@@ -14,7 +14,7 @@ cursor = connection.cursor()
 
 for i,row in empdata.iterrows():
             #here %S means string values 
-            sql = "INSERT INTO cataloguedata (image_url, item_name, item_price, average_rating, number_of_ratings) VALUES (%s,%s,%s,%s,%s)"
+            sql = "INSERT INTO cataloguedata (product_url, image_url, item_name, item_price, average_rating, number_of_ratings) VALUES (%s,%s,%s,%s,%s,%s)"
             cursor.execute(sql, tuple(row))
             print("Record inserted")
             # the connection is not auto committed by default, so we must commit to save our changes
