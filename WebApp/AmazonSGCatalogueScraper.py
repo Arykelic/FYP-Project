3fc891c7292e666f861e2f7beafbdde091e3a176
Dominic Loh <dominic.loh@hotmail.com>
2022-04-29 05:01:15 +0800

    test

--- WebApp/AmazonSGCatalogueScraper.py
+++ WebApp/AmazonSGCatalogueScraper.py
@@ -28,7 +28,9 @@ def getnextpage(soup):
         page = soup.find("span", {"class": "s-pagination-strip"})
         # if not last page
         if not page.find("span", {"class": "s-pagination-item s-pagination-next s-pagination-disabled "}):
-            url = "https://www.amazon.sg" + str(page.find("a", {"class": "s-pagination-item s-pagination-next s-pagination-button s-pagination-separator"})["href"])
+            url = "https://www.amazon.sg" + \
+                str(page.find("a", {
+                    "class": "s-pagination-item s-pagination-next s-pagination-button s-pagination-separator"})["href"])
             return url
         else:
             return
@@ -40,7 +42,8 @@ def getnextpage(soup):
 # conversion of data into CSV
 
 # Change Directory
-#os.chdir('AmazonSGCatalogueFiles')
+# os.chdir('AmazonSGCatalogueFiles')
+
 
 #filename = "{}+Catalogue.csv".format(search_term)
 filename = "food+Catalogue.csv"
@@ -54,10 +57,11 @@ while True:
     soup = getdata(url)
     # pulling all data sets on current page and verifying length
     # create containers group
-    containers = soup.findAll("div", {"class": "sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 sg-col s-widget-spacing-small sg-col-4-of-20"})
+    containers = soup.findAll("div", {
+                              "class": "sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 sg-col s-widget-spacing-small sg-col-4-of-20"})
     # use below line to check the length of the dataset
     print(len(containers))
-    #containers
+    # containers
     #filename = "{}_Catalogue.csv".format(url).replace("/",",").replace("?",",")
     #f = open(filename, "w", encoding="utf-8")
 
@@ -67,23 +71,30 @@ while True:
 
     # loop inside each container
     for container in containers:
-        Product_Url_Container = container.findAll("a", {"class": "a-link-normal s-no-outline"})
-        Product_Url = "https://www.amazon.sg" + str(Product_Url_Container[0]["href"])
+        Product_Url_Container = container.findAll(
+            "a", {"class": "a-link-normal s-no-outline"})
+        Product_Url = "https://www.amazon.sg" + \
+            str(Product_Url_Container[0]["href"])
 
-        Image_Url_Container = container.findAll("div", {"class": "a-section aok-relative s-image-square-aspect"})
+        Image_Url_Container = container.findAll(
+            "div", {"class": "a-section aok-relative s-image-square-aspect"})
         Image_Url = Image_Url_Container[0].img["src"]
 
-        Item_Name_Container = container.findAll("span", {"class": "a-size-base a-color-base a-text-normal"})
+        Item_Name_Container = container.findAll(
+            "span", {"class": "a-size-base a-color-base a-text-normal"})
         Item_Name = Item_Name_Container[0].text
 
         try:
-            Item_Price_Container = container.findAll("span", {"class": "a-offscreen"})
+            Item_Price_Container = container.findAll(
+                "span", {"class": "a-offscreen"})
             Item_Price = Item_Price_Container[0].text[2:]
 
-            Average_Rating_Container = container.findAll("span", {"class": "a-icon-alt"})
+            Average_Rating_Container = container.findAll(
+                "span", {"class": "a-icon-alt"})
             Average_Rating = Average_Rating_Container[0].text[0:4]
 
-            Number_Of_Ratings_Container = container.findAll("span", {"class": "a-size-base s-underline-text"})
+            Number_Of_Ratings_Container = container.findAll(
+                "span", {"class": "a-size-base s-underline-text"})
             Number_Of_Ratings = Number_Of_Ratings_Container[0].text
 
         except:
