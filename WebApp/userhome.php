8ffb4ea301826072c223058f9ec8f2059641f4e0
Dominic Loh <dominic.loh@hotmail.com>
2022-04-30 04:40:41 +0800

    testing the integration and final production phase

--- WebApp/userhome.php
+++ WebApp/userhome.php
@@ -230,7 +230,8 @@ include "PageDataConfig.php"
               echo "<br><br>" . $app_data; */
               
                     /* $command = system("python AmazonSGCatalogueScraper.py" . $_GET["cataloguescraper"]); */
-                    $command =  escapeshellcmd('python WebApp/AmazonSGCatalogueScraper.py');
+                    $input = $_POST["cataloguescraper"];
+                    $command =  escapeshellcmd("python AmazonSGCatalogueScraper.py '$input' 2>&1");
                     $result = shell_exec($command);
                     echo "<pre>";
                     print_r($result);
