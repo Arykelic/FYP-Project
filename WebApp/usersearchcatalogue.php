e9a67236d15ee3d0ae29c70fb6670147aee768c7
Dominic Loh <dominic.loh@hotmail.com>
2022-05-07 08:47:05 +0800

    final phase of development

--- WebApp/usersearchcatalogue.php
+++ WebApp/usersearchcatalogue.php
@@ -164,6 +164,7 @@ function filterTableCount($count)
         <div class="card-body" width="100%">
           <!-- <div class="table-responsive"> -->
           <div>Number of Results: <?php echo $data['count'] ?></div>
+          <div>Number of Results: <?php echo $data ?></div>
           <br>
           <div class="table table-bordered table-striped" style="text-align:left;" width="100%" cellspacing="0">
             <table>
