a34416c1380833c355403c2722ca035dab9afbf2
Dominic Loh <dominic.loh@hotmail.com>
2022-05-10 22:15:01 +0800

    Final deployements

--- WebApp/userpagescraper.php
+++ WebApp/userpagescraper.php
@@ -14,7 +14,7 @@ include "CatalogueConfig.php";
 include "CombinedReviewConfig.php";
 include "PageDataConfig.php";
 
-$pagescraper = "";
+
 $pagescraper_err = "";
 $urlregex = "/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/";
 
