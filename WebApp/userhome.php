6b0814599153bbd3c0939e1f6567ff51e3a0131f
Dominic Loh <dominic.loh@hotmail.com>
2022-05-10 22:08:53 +0800

    Final deployements

--- WebApp/userhome.php
+++ WebApp/userhome.php
@@ -87,7 +87,7 @@ include "PageDataConfig.php";
         <!-- <label class="info" style="text-align:left;"> -->
           <!-- <img class="info" src="elogofinal.png" height="150" width="150"></img> -->
           <img src="elogofinal.png" style="text-align:left; height: auto; width: 40px;"></img>
-          E-Commerce Insight (User)(Home)
+          <div id="Title">E-Commerce Insight (User)(Home)</div>
           <!-- <span style="width: 70px;"><img src="elogofinal.png"></img></span> -->
         <!-- </label> -->
 
